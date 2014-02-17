7e86a427c7cc183e93ba8a5b81c091d612fac646
Alexandru Plugaru <alexandru@gymglish.com>
2014-02-17 17:35:45 +0100

    Removed useless function

--- jquery.specialinput.js
+++ jquery.specialinput.js
@@ -183,39 +183,6 @@
             after_elem.after(keyboard);
         }
 
-        function getSelectionBoundaryElement(isStart) {
-            var range, sel, container;
-            if (document.selection) {
-                range = document.selection.createRange();
-                range.collapse(isStart);
-                return range.parentElement();
-            } else {
-                sel = window.getSelection();
-                if (sel.getRangeAt) {
-                    if (sel.rangeCount > 0) {
-                        range = sel.getRangeAt(0);
-                    }
-                } else {
-                    // Old WebKit
-                    range = document.createRange();
-                    range.setStart(sel.anchorNode, sel.anchorOffset);
-                    range.setEnd(sel.focusNode, sel.focusOffset);
-
-                    // Handle the case when the selection was selected backwards (from the end to the start in the document)
-                    if (range.collapsed !== sel.isCollapsed) {
-                        range.setStart(sel.focusNode, sel.focusOffset);
-                        range.setEnd(sel.anchorNode, sel.anchorOffset);
-                    }
-                }
-
-                if (range) {
-                    container = range[isStart ? "startContainer" : "endContainer"];
-
-                    // Check if the container is a text node and return its parent if so
-                    return container.nodeType === 3 ? container.parentNode : container;
-                }   
-            }
-        }
 
         function buttonClicked() {
             var self = $(this),
@@ -233,14 +200,14 @@
                     var res = "";
 
                     if (typeof selectionStart === 'number'){
-                        var chars = inputVal.split("")
+                        var chars = inputVal.split("");
                         chars.splice(selectionStart, 0, new_value);
                         res = chars.join("");
-                        selectionStart = res.length; 
+                        selectionStart = res.length;
                     } else {
                         res = inputVal + new_value;
                     }
-                    
+
                     input.val(res).trigger(e);
                     if (!e.isDefaultPrevented()) {
                         input.focus();
